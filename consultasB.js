3e1f18bfc8fbb89ba1a33cc42ff4b6e515c723f4
Jossefa <javierarodriguezvera@gmail.com>
2022-05-31 22:46:10 -0400

    commit metodos

--- consultasB.js
+++ consultasB.js
@@ -1,22 +1,58 @@
-const conexion = require ('./conexion')
+const conexion = require('./conexion')
 
-const consultasB = async() => {
+const consultasB = async () => {
     const cnx = await conexion()
-
-    const insertarUsuario = async(nombre, balance) => {
+    // METODO insertar nuevo usuario
+    const insertarUsuario = async (nombre, balance) => {
         const SQLQuery = {
             text: 'INSERT INTO usuarios(nombre, balance) VALUES ($1, $2) RETURNING *',
             values: [nombre, balance]
         }
         try {
-            const data = await cnx.client.query(SQLQuery)
-            return data
-            
+            const datos = await cnx.client.query(SQLQuery)
+            //cnx.release()
+            //cnx.pool.end()
+            return datos
+
         } catch (err) {
-            console.error(err)
-            
+            throw new Error(err)
         }
+
+    }
+    // METODO obtener todos los usuarios 
+    const obtenerUsuarios = async () => {
+        const SQLQuery = {
+            text: 'SELECT * FROM usuarios',
+        }
+        try {
+            const datos = await cnx.client.query(SQLQuery)
+            //cnx.release()
+           // cnx.pool.end()
+            return datos.rows
+        } catch (err) {
+            throw new Error(err)
+        }
+
     }
 
-    return {insertarUsuario}
+    const modificarUsuario = async (nombre, balance, id) => {
+        const SQLQuery = {
+            text: 'UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = $3',
+            values: [nombre, balance, id]
+        }
+        try {
+            console.log(SQLQuery)
+            const datos = await cnx.client.query(SQLQuery)
+            //cnx.release()
+            //cnx.pool.end()
+            return datos
+        } catch (err) {
+            throw new Error(err)
+        }
+
+    }
+
+    return { insertarUsuario, obtenerUsuarios, modificarUsuario }
 }
+
+module.exports = consultasB
--- index.js
+++ index.js
@@ -1,42 +1,81 @@
-const fs = require ('fs')
-const url = require ('url')
-const http = require ('http')
-//const conexion = require ('./conexion')
+const fs = require('fs')
+const url = require('url')
+const http = require('http')
+const consultasB = require('./consultasB')
+const conexion = require('./conexion')
 
 const servidor = http.createServer(async (req, res) => {
- const method = req.method
- const urlb = req.url
+    const method = req.method
+    const urlb = req.url
+    const consults = await consultasB()
 
-    if(urlb === '/' && method === 'GET') {
+    if (urlb === '/' && method === 'GET') {
         res.setHeader('Content-Type', 'text/html')
         res.end(fs.readFileSync('index.html', 'utf-8'))
     }
-    else if(urlb === '/usuario' && method === 'POST') {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 201
-        res.end('{}')
+    else if (urlb === '/usuario' && method === 'POST') {
+        try {
+            let body
+            req.on('data', (chunk) => {
+                body = chunk.toString()
+            })
+            req.on('end', async () => {
+                const datos = JSON.parse(body)
+                const resp = await consults.insertarUsuario(datos.nombre, datos.balance)
+                res.setHeader('Content-Type', 'application/json')
+                res.statusCode = 201
+                res.end(JSON.stringify(resp))
+            })
+        } catch (error) {
+            res.statusCode = 500
+            res.end(`message: ${error}`)
+        }
+
     }
-    else if(urlb.startsWith('/usuarios') && method === 'GET') {
+    else if (urlb.startsWith('/usuarios') && method === 'GET') {
+        try {
+            const resp = await consults.obtenerUsuarios()
+            res.setHeader('Content-Type', 'application/json')
+            res.statusCode = 200
+            res.end(JSON.stringify(resp))
+
+        } catch (error) {
+            res.statusCode = 500
+            res.end(`message: ${error}`)
+        }
+
+    }
+    else if (urlb.startsWith('/usuario') && method === 'PUT') {
+        try {
+            const id = url.parse(urlb, true).query
+            let body
+            req.on('data', (chunk) => {
+                body = chunk.toString()
+            })
+            req.on('end', async () => {
+                const datos = JSON.parse(body)
+                const resp = await consults.modificarUsuario(datos.name, datos.balance, id.id)
+
+                res.setHeader('Content-Type', 'application/json')
+                res.statusCode = 200
+                res.end(JSON.stringify(resp))
+            })
+        } catch (error) {
+            res.statusCode = 500
+            res.end(`message: ${error}`)
+        }
+    }
+    else if (urlb.startsWith('/usuario') && method === 'DELETE') {
         res.setHeader('Content-Type', 'application/json')
         res.statusCode = 200
         res.end('{}')
     }
-    else if(urlb.startsWith('/usuario') && method === 'PUT') {
+    else if (urlb === '/transferencia' && method === 'POST') {
         res.setHeader('Content-Type', 'application/json')
         res.statusCode = 200
         res.end('{}')
     }
-    else if(urlb.startsWith('/usuario') && method === 'DELETE') {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 200
-        res.end('{}')
-    }
-    else if(urlb === '/transferencia' && method === 'POST') {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 200
-        res.end('{}')
-    }
-    else if(urlb === '/transferencias' && method === 'GET') {
+    else if (urlb === '/transferencias' && method === 'GET') {
         res.setHeader('Content-Type', 'application/json')
         res.statusCode = 200
         res.end('{}')
@@ -48,7 +87,7 @@ const servidor = http.createServer(async (req, res) => {
     }
 
 
-    })
-    servidor.listen(3000, () => console.log('Servidor iniciado en http://localhost:3000'))
+})
+servidor.listen(3000, () => console.log('Servidor iniciado en http://localhost:3000'))
 
-  
+
